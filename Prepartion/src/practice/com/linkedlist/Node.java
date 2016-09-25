28246261aba0aa80c8ba6904e3b993b964e7ad90
Akshay <adsiddhpura@hotmail.com>
2016-09-25 08:46:58 +0530

    Linkedlist
    add element method added.

---
+++ Prepartion/src/practice/com/linkedlist/Node.java
@@ -0,0 +1,18 @@
+package practice.com.linkedlist;
+
+public class Node {
+
+	
+	Node next;
+	Node prev;
+	int data;
+	/**
+	 * @param data
+	 */
+	public Node(int data) {
+		super();
+		this.data = data;
+	}
+	
+	
+}
